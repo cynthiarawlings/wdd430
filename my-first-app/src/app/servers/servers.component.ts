88e332d54feab45d1ab0e2dd4fddc803e7ceb04f
hyruleismyrule <hyruleismyrule@gmail.com>
2023-01-10 18:27:15 -0500

    the basics complete

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!"
+  serverName = "Test Server";
+  serverCreated = false;
+  servers = ["Test Server", "Test Server 2"];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }
