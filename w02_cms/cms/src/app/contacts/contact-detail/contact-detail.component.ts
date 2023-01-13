01cb5ebd00f39e52e37a50790cf7b54724ad4058
hyruleismyrule <hyruleismyrule@gmail.com>
2023-01-13 15:23:40 -0500

    assignment complete

--- w02_cms/cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ w02_cms/cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-detail',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent {
-
+  detailContact: Contact[];
 }
