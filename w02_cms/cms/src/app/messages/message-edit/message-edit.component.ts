e7b59e9bd9dac3e21faa9674d7b651d3957c6b2a
hyruleismyrule <hyruleismyrule@gmail.com>
2023-01-20 15:46:57 -0500

    assignment complete

--- w02_cms/cms/src/app/messages/message-edit/message-edit.component.ts
+++ w02_cms/cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent {
+  @ViewChild("subject") subjectRef: ElementRef;
+  @ViewChild("msgText") msgTextRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
+  onSendMessage() {
+    const currentSender = "Cynthia Rawlings";
+    let id = "1";
+
+    const subject = this.subjectRef.nativeElement.value;
+    const msgText = this.msgTextRef.nativeElement.value;
+    const sender = currentSender;
+    const newMessage = new Message(id, subject, msgText, sender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectRef.nativeElement.value = "";
+    this.msgTextRef.nativeElement.value = "";
+  }
 }
--- w02_cms/cms/src/app/messages/message-list/message-list.component.ts
+++ w02_cms/cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message("0", "Test 0", "This is text 0", "Me"),
+    new Message("1", "Test 1", "This is text 1", "Me"),
+    new Message("2", "Test 2", "This is text 2", "Me")
+  ];
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
