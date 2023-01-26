69a44a23ac8943edc42f1b1a8bdf4bd523f6ca01
hyruleismyrule <hyruleismyrule@gmail.com>
2023-01-26 13:19:15 -0500

    documents component

--- w02_cms/cms/src/app/documents/document-list/document-list.component.ts
+++ w02_cms/cms/src/app/documents/document-list/document-list.component.ts
@@ -10,9 +10,12 @@ export class DocumentListComponent {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
   documents: Document[] = [
-    new Document("1", "Document1", "Test Document", "#", []),
-    new Document("2", "Document1", "Test Document", "#", []),
-    new Document("3", "Document1", "Test Document", "#", [])
+    new Document("1", "GS170 - Career Development", "Test Document 1", "#", []),
+    new Document("2", "CSE220C - C++ Language", "Test Document 2", "#", []),
+    new Document("3", "CIT110 - Introduction to Excel", "Test Document 3", "#", []),
+    new Document("4", "MKT250 - Web Business Creation", "Test Document 4", "#", []),
+    new Document("5", "WDD430 - Web Full-Stack Development", "Test Document 5", "#", []),
+    new Document("6", "CIT490 - Senior Project", "Test Document 6", "#", [])
   ];
 
   onSelectedDocument(document: Document) {
