08e70e9f6d6e1dd2ef84957067d5418e413ecf80
hyruleismyrule <hyruleismyrule@gmail.com>
2023-01-26 12:40:49 -0500

    document list component

--- w02_cms/cms/src/app/documents/document-list/document-list.component.ts
+++ w02_cms/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document("1", "Document1", "Test Document", "#", []),
+    new Document("2", "Document1", "Test Document", "#", []),
+    new Document("3", "Document1", "Test Document", "#", [])
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
