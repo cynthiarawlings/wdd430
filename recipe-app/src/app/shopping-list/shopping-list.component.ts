b8aed5419b5acc05500b0042b43d5e2e90d3235f
hyruleismyrule <hyruleismyrule@gmail.com>
2023-02-21 13:48:00 -0500

    recipe list form

--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -25,6 +25,10 @@ export class ShoppingListComponent {
     );
   }
 
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
