85905050a443ceef033c6014f8cfe1193b160bb2
hyruleismyrule <hyruleismyrule@gmail.com>
2023-02-23 12:06:55 -0500

    w08 assignment

--- w02_cms/cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ w02_cms/cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -38,6 +38,7 @@ export class ContactEditComponent {
         this.contact = JSON.parse(JSON.stringify(this.originalContact));
         if (this.contact.group) {
           this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
+          console.log(this.groupContacts);
         }
       }
     );
@@ -59,6 +60,38 @@ export class ContactEditComponent {
     this.router.navigate(['/contacts']);
   }
 
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {// newContact has no value
+      return true;
+    }
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  addToGroup($event: any) {
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+  }
+
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+    this.groupContacts.splice(index, 1);
+  }
 }
 
 
