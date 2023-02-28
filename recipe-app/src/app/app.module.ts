7cf62a6c52bd4f8164f14bf7ff47e5d5cb1fc07f
hyruleismyrule <hyruleismyrule@gmail.com>
2023-02-28 15:52:10 -0500

    did w09 recipes, but it is broken for some reason

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,7 +16,6 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
 
 
@@ -34,9 +35,10 @@ import { RecipeService } from './recipes/recipe.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -9,23 +9,25 @@ export class RecipeService {
     recipeSelected = new Subject<Recipe>();
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            "A Test Recipe", 
-            "This is simply a test", 
-            "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg",
-            [
-                new Ingredient("Meat", 1),
-                new Ingredient("French Fries", 20)
-            ]),
-        new Recipe("Another Test Recipe", 
-            "This is simply a test", 
-            "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg",
-            [
-                new Ingredient("Meat", 1),
-                new Ingredient("French Fries", 20)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         "A Test Recipe", 
+    //         "This is simply a test", 
+    //         "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg",
+    //         [
+    //             new Ingredient("Meat", 1),
+    //             new Ingredient("French Fries", 20)
+    //         ]),
+    //     new Recipe("Another Test Recipe", 
+    //         "This is simply a test", 
+    //         "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg",
+    //         [
+    //             new Ingredient("Meat", 1),
+    //             new Ingredient("French Fries", 20)
+    //         ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {
 
@@ -56,6 +58,12 @@ export class RecipeService {
     deleteRecipe(index: number) {
         this.recipes.splice(index, 1);
         this.recipesChanged.next(this.recipes.slice());
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+        // The recipes are not showing up
     }
 
 }
